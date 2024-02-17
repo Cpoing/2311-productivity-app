dc809d96539d7cdf6a660343cb9d5fd31d87f128
Taehyun Kim <taehyun1834@gmail.com>
2024-02-16 23:53:29 -0500

    Create ScoreCounterTest.java

---
+++ demo/src/main/java/com/example/test/ScoreCounterTest.java
@@ -0,0 +1,60 @@
+package main.java.com.example.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import com.example.Components.ScoreCounter;
+
+class ScoreCounterTest {
+
+	@Test
+	void test_1() {
+		ScoreCounter sc = new ScoreCounter();
+		sc.addScore("Low");
+		assertEquals(100, sc.getCounter());
+	}
+	
+	@Test
+	void test_2() {
+		ScoreCounter sc = new ScoreCounter();
+		sc.addScore("Medium");
+		assertEquals(300, sc.getCounter());
+	}
+	
+	@Test
+	void test_3() {
+		ScoreCounter sc = new ScoreCounter();
+		sc.addScore("High");
+		assertEquals(500, sc.getCounter());
+	}
+	
+	@Test
+	void test_4() {
+		ScoreCounter sc = new ScoreCounter();
+		sc.subtractScore("Low");
+		assertEquals(-100, sc.getCounter());
+	}
+	
+	@Test
+	void test_5() {
+		ScoreCounter sc = new ScoreCounter();
+		sc.subtractScore("Medium");
+		assertEquals(-300, sc.getCounter());
+	}
+	
+	@Test
+	void test_6() {
+		ScoreCounter sc = new ScoreCounter();
+		sc.subtractScore("High");
+		assertEquals(-500, sc.getCounter());
+	}
+	
+	@Test
+	void test_7() {
+		ScoreCounter sc = new ScoreCounter();
+		sc.addScore("High");
+		sc.addScore("Medium");
+		sc.subtractScore("High");
+		assertEquals(300, sc.getCounter());
+	}
+}
